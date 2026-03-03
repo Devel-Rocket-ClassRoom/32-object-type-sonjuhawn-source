a5f46af2fcd1e698526fe630d766b1763ceffb6b
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-04 00:08:19 +0900

    연습문제 완료

--- CodingPractice/Program.cs
+++ CodingPractice/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.ObjectiveC;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 // README.md를 읽고 코드를 작성하세요.
 
@@ -95,7 +96,6 @@ for (int a = 0; a < 1000; a++)
 }
 Console.WriteLine($"박싱 사용: {sum1}");
 
-// 좋은 예: 박싱 없이 직접 처리
 int sum2 = 0;
 for (int r = 0; r < 1000; r++)
 {
@@ -111,6 +111,81 @@ GenericStack<int> intStack = new GenericStack<int>();
 intStack.Push(42);       
 int value = intStack.Pop();  
 Console.WriteLine(value);
+
+Console.WriteLine("'''\n");
+
+
+Console.WriteLine("'''");
+int q = 123;
+string hel = "hello";
+char c = 'a';
+double ar = 3.14;
+object ob = new object();
+
+Console.WriteLine(q.GetType());  
+Console.WriteLine(hel.GetType());  
+Console.WriteLine(c.GetType());  
+Console.WriteLine(ar.GetType()); 
+Console.WriteLine(ob.GetType());  
+
+Console.WriteLine("'''\n");
+
+Console.WriteLine("'''");
+Console.WriteLine(typeof(int));        
+Console.WriteLine(typeof(string));     
+Console.WriteLine(typeof(double));
+
+Console.WriteLine("'''\n");
+
+Console.WriteLine("'''");
+Point1 p1 = new Point1 { X = 10, Y = 20 };
+Console.WriteLine(p1.GetType().Name);              
+Console.WriteLine(typeof(Point).Name);            
+Console.WriteLine(p1.GetType() == typeof(Point));  
+Console.WriteLine(p1.X.GetType().Name);            
+Console.WriteLine(p1.Y.GetType().FullName);
+
+Console.WriteLine("'''\n");
+
+Console.WriteLine("'''");
+int ab = 42;
+double pi = 3.14;
+bool tr = true;
+Console.WriteLine(ab.ToString()); 
+Console.WriteLine(pi.ToString());      
+Console.WriteLine(tr.ToString());
+
+Console.WriteLine("'''\n");
+
+Console.WriteLine("'''");
+Panda panda = new Panda();
+panda.Name = "Petey";
+Console.WriteLine(panda);
+Console.WriteLine(panda.ToString());
+
+Console.WriteLine("'''\n");
+
+Console.WriteLine("'''");
+Player player = new Player() { Name = "Hero", Level = 10, Health = 100 };
+Console.WriteLine(player.ToString());
+Console.WriteLine("'''\n");
+
+Console.WriteLine("'''");
+string s1 = "Hello";
+string s2 = "Hello";
+string s3 = s1;
+
+Console.WriteLine($"ReferenceEquals(s1, s2): {object.ReferenceEquals(s1, s2)}"); 
+Console.WriteLine($"ReferenceEquals(s1, s3): {object.ReferenceEquals(s1, s3)}");  
+
+// 새 객체 생성 시
+object ob1 = new object();
+object ob2 = new object();
+object ob3 = obj1;
+
+Console.WriteLine($"obj1.Equals(obj2): {ob1.Equals(obj2)}");           
+Console.WriteLine($"ReferenceEquals(obj1, obj2): {object.ReferenceEquals(ob1, obj2)}");  
+Console.WriteLine($"ReferenceEquals(obj1, obj3): {object.ReferenceEquals(ob1, obj3)}");  
 
 Console.WriteLine("'''\n");
 
@@ -153,5 +228,33 @@ class GenericStack<T>
     public T Pop()
     {
         return _items[--_top];
+    }
+}
+
+class Point1
+{
+    public int X;
+    public int Y;
+}
+
+class Panda
+{
+    public string Name;
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+
+class Player
+{
+    public string Name;
+    public int Level;
+    public int Health;
+
+    public override string ToString()
+    {
+        return $"Player {{ Name = {Name}, Level = {Level}, Health = {Health} }}";
     }
 }
