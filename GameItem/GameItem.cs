303ddf7040aa456b79162d9e2363580f9b9ed057
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-04 22:07:02 +0900

    과제완료

---
+++ GameItem/GameItem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Item
+{
+    public string Name;
+    public int Price;
+    public Item(string name, int price)
+    {
+        Name = name;
+        Price = price;
+    }
+    public override string ToString()
+    {
+        return "아이템 정보 출력";
+    }
+}
+class Weapon : Item
+{
+    public int Damage;
+    public Weapon(string name, int price, int damage) : base(name, price)
+    {
+        Damage = damage;
+    }
+    public override string ToString()
+    {
+        return $"Weapon {{ Name = {Name} Price = {Price}, Damage = {Damage} }} [Weapon]";
+    }
+}
+
+class Potion : Item
+{
+    public int HealthAmount;
+    public Potion(string name, int price, int healthamount) : base(name, price)
+    {
+        HealthAmount = healthamount;
+    }
+    public override string ToString()
+    {
+        return $"Potion {{ Name = {Name} Price = {Price}, HealAmount = {HealthAmount} }} [Potion]";
+    }
+}
+
+class Inventory
+{
+    public object[] objects = new object[10];
+    int count = 0;
+    public void AddItem(object item)
+    {
+        if (count < 10)
+        {
+            objects[count] = item;
+            count++;
+        }
+        else
+        {
+            Console.WriteLine("인벤토리가 가득 찼습니다");
+        }
+    }
+    public void ShowInventory()
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (objects[i] != null)
+            {
+                Console.Write($"슬롯 {count}: ");
+                Console.WriteLine(objects[i].ToString());
+            }
+        }
+    }
+}
--- GameItem/Program.cs
+++ GameItem/Program.cs
@@ -1,22 +1,22 @@
 using System;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine("=== 인벤토리 시스템 테스트 ===");
+Inventory inventory = new Inventory();
+Weapon weapon1 = new Weapon("불꽃 검", 500, 25);
+Weapon weapon2 = new Weapon("얼음 활", 450, 20);
+Potion potion1 = new Potion("체력 물약", 50, 100);
+Potion potion2 = new Potion("마나 물약", 80, 50);
 
-class Item
-{
-    public string Name;
-    public int Price;
-    public override string ToString()
-    {
-        return 
-    }
-}
-class Weapon : Item
-{
-    public int Damage;
-    public override string ToString()
-    {
-        return $"";
-    }
-}
+inventory.AddItem(weapon1);
+inventory.AddItem(weapon2);
+inventory.AddItem(potion1);
+inventory.AddItem(potion2);
+inventory.ShowInventory();
+
+Console.WriteLine("=== 타입 확인 테스트 ===");
+Console.WriteLine($"sword의 타입: {weapon1.GetType()}");
+Console.WriteLine($"sword.GetType() == typeof(Weapon): {weapon1.GetType() == typeof(Weapon)}");
+Console.WriteLine($"sword.GetType() == typeof(Item): {weapon1.GetType() == typeof(Item)}");
+Console.WriteLine($"sword is Item: {weapon1 is Item}");
+
