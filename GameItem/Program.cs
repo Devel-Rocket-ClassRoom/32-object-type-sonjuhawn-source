7b348508fd2d63ff3297ed02ec751d8b954a689a
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-04 09:18:04 +0900

    게임 아이템 막힘

--- GameItem/Program.cs
+++ GameItem/Program.cs
@@ -2,3 +2,21 @@ using System;
 
 // README.md를 읽고 코드를 작성하세요.
 Console.WriteLine("코드를 작성하세요.");
+
+class Item
+{
+    public string Name;
+    public int Price;
+    public override string ToString()
+    {
+        return 
+    }
+}
+class Weapon : Item
+{
+    public int Damage;
+    public override string ToString()
+    {
+        return $"";
+    }
+}
